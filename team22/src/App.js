b9e7e886bebb0436151c72cd8344a85a2bbfb7cb
amar-phull <amardeepphull7@gmail.com>
2024-11-27 19:37:40 +0000

    4 - frontend navbar created

--- team22/src/App.js
+++ team22/src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import EmpTodoList from './employee/todolist/EmpTodoList';
+import Empnavbar from './employee/Empnavbar/Empnavbar';
 
 function App() {
-  // zaid was here.
   return (
-    <EmpTodoList />
+    <div className="d-flex">
+      {/* Sidebar on the left */}
+      <Empnavbar />
+
+      {/* Main Content */}
+      <div
+        className="flex-grow-1 d-flex justify-content-center"
+        style={{ marginLeft: "250px", paddingTop: "20px", minHeight: "100vh" }}
+      >
+        <EmpTodoList />
+      </div>
+    </div>
   );
 }
 
--- team22/src/employee/Empnavbar/Empnavbar.js
+++ team22/src/employee/Empnavbar/Empnavbar.js
@@ -1,1 +1,68 @@
-// this is where the navbar code will go 
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Empnavbar = () => {
+  return (
+    <div
+      className="bg-dark text-white vh-100"
+      style={{
+        width: "250px", // Adjusted sidebar width
+        position: "fixed",
+        top: "0",
+        left: "0",
+        height: "100vh",
+        overflowY: "auto",
+      }}
+    >
+      <h3 className="text-center py-3">Sidebar</h3>
+      <ul className="nav flex-column px-3">
+        <li className="nav-item">
+          <a
+            href="#home"
+            className="nav-link text-white"
+            style={{ padding: "10px 15px" }}
+            onMouseEnter={(e) => (e.target.style.backgroundColor = "#495057")}
+            onMouseLeave={(e) => (e.target.style.backgroundColor = "transparent")}
+          >
+            Home
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#projects-tasks"
+            className="nav-link text-white"
+            style={{ padding: "10px 15px" }}
+            onMouseEnter={(e) => (e.target.style.backgroundColor = "#495057")}
+            onMouseLeave={(e) => (e.target.style.backgroundColor = "transparent")}
+          >
+            Projects/Tasks
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#forum"
+            className="nav-link text-white"
+            style={{ padding: "10px 15px" }}
+            onMouseEnter={(e) => (e.target.style.backgroundColor = "#495057")}
+            onMouseLeave={(e) => (e.target.style.backgroundColor = "transparent")}
+          >
+            Forum
+          </a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="#todolist"
+            className="nav-link text-white"
+            style={{ padding: "10px 15px" }}
+            onMouseEnter={(e) => (e.target.style.backgroundColor = "#495057")}
+            onMouseLeave={(e) => (e.target.style.backgroundColor = "transparent")}
+          >
+            Todolist
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Empnavbar;
