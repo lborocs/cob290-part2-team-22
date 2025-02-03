f600cbfba7b89b0484e13399ee1cbe90e9b93f93
amar-phull <amardeepphull7@gmail.com>
2025-02-03 18:15:36 +0000

    12 - made settings pages

---
+++ team22/src/employee/EmpSettings/EmpSettings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const EmpSettings = () => {
+  return (
+    <div>
+      <h1 className="text-center">Welcome to the employee's setting page</h1>
+      <p className="text-center">This is the settings content.</p>
+    </div>
+  );
+};
+
+export default EmpSettings;
---
+++ team22/src/manager/ManSettings/ManSettings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const ManSettings = () => {
+  return (
+    <div>
+      <h1 className="text-center">Welcome to the manager's setting page</h1>
+      <p className="text-center">This is the settings content.</p>
+    </div>
+  );
+};
+
+export default ManSettings;
